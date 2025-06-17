48fabf1506c7532be01d9ce7949d7c768ca451db
deshyam01 <shyamsundargayen345@gmail.com>
2025-06-17 20:34:25 +0530

    Navbar upload

---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div class="navbar">
+      <nav>
+        <div class="logo">
+          <h1>ZapQuiz</h1>
+        </div>
+        <input type="checkbox" name="hamburger" id="hamburger" />
+        <label for="hamburger">☰</label>
+        <div class="navigation">
+          <button>Home</button>
+          <button>Start Quiz</button>
+          <button>Scores</button>
+          <button>About</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./Navbar";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>
+);
