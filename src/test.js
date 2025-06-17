276a448eba2ff3e80bb71309c6746e02ad8e8e6f
deshyam01 <shyamsundargayen345@gmail.com>
2025-06-17 11:24:08 +0530

    clearing main files

--- src/test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// A simple functional component
-const Hello = () => <h1>Hello, world!</h1>;
-
-// Render the component into the root div
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hello />);
