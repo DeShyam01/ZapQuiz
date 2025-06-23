6a5ce333ddac6fa4267efa4e558036722af7ed81
deshyam01 <shyamsundargayen345@gmail.com>
2025-06-23 17:14:51 +0530

    fix for deploy

--- src/App.js
+++ src/App.js
@@ -10,67 +10,62 @@ import AboutPage from "./components/AboutPage";
 import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/Home",
-      element: (
-        <>
-          <Navbar />
-          <Home />
-        </>
-      ),
-    },
-    {
-      path: "/Quiz",
-      element: (
-        <>
-          <Navbar />
-          <Quiz />
-        </>
-      ),
-    },
-    {
-      path: "/Quiz/Start",
-      element: <QuizEngine />,
-    },
-    {
-      path: "/Score",
-      element: (
-        <>
-          <Navbar />
-          <Score />
-        </>
-      ),
-    },
-    {
-      path: "/Leaderboard",
-      element: (
-        <>
-          <Navbar />
-          <Leaderboard />
-        </>
-      ),
-    },
-    {
-      path: "/About",
-      element: (
-        <>
-          <Navbar />
-          <AboutPage />
-          <Footer />
-        </>
-      ),
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: "/ZapQuiz",
-  }
-);
+    path: "/Home",
+    element: (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    ),
+  },
+  {
+    path: "/Quiz",
+    element: (
+      <>
+        <Navbar />
+        <Quiz />
+      </>
+    ),
+  },
+  {
+    path: "/Quiz/Start",
+    element: <QuizEngine />,
+  },
+  {
+    path: "/Score",
+    element: (
+      <>
+        <Navbar />
+        <Score />
+      </>
+    ),
+  },
+  {
+    path: "/Leaderboard",
+    element: (
+      <>
+        <Navbar />
+        <Leaderboard />
+      </>
+    ),
+  },
+  {
+    path: "/About",
+    element: (
+      <>
+        <Navbar />
+        <AboutPage />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 const App = () => {
   return <RouterProvider router={router} />;
